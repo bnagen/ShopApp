65e30cb9922ed0b655fea19332a4e1fd07eacd9a
br <dxrock1@gmail.com>
2022-03-28 11:15:37 -0700

    started working on frontend elements

---
+++ frontend/src/components/Footer.js
@@ -0,0 +1,13 @@
+import { Container, Row, Col } from "react-bootstrap"
+
+const Footer = () => {
+  return (
+    <Container>
+      <Row>
+        <Col classname='text-center py-3'>Copyright &copy; The Shop</Col>
+      </Row>
+    </Container>
+  )
+}
+
+export default Footer
